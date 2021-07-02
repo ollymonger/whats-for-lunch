8d46746f76a0eb6cc303a6e2ec0fbaa23810594c
Olly Monger <olivermonger123@gmail.com>
2021-07-02 12:13:07 +0100

    Added models testing

--- src/nullFoodObject.test.tsx
+++ src/nullFoodObject.test.tsx
@@ -1,5 +1,6 @@
 import { getRandomFood } from "./utils/getRandomFood";
 import { Food } from './models/Food'
+import { Snack } from './models/Snack'
 
 test('getRandomFood returns food object with null snack, drink and mains', async () => {
     let returnedFood = await getRandomFood();
@@ -24,4 +25,28 @@ test('_setFood sets error to true, to enable the error component!', async () => 
     _setFood(defaultFood);
 
     expect(error).toBe(true);
+})
+
+
+
+test('expect null Snack Object to be inputted to Food object as null', () => {
+
+    let snack: Snack = {
+        name: "",
+        picture: "",
+        preptime: 0,
+        info: ""
+    }
+
+    let Food: Food = {
+        snack: snack,
+        main: null,
+        drink: null
+    }
+
+    if (snack.name === "" || snack.picture === "null" || snack.preptime === 0 || snack.info === "") {
+        Food.snack = null;
+    }
+
+    expect(Food.snack).toBe(null);
 })
