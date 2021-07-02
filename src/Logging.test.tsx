878256a3c1f701a10071dac0dcc81245c381d80e
Olly Monger <olivermonger123@gmail.com>
2021-07-02 01:31:00 +0100

    Added logging test

---
+++ src/Logging.test.tsx
@@ -0,0 +1,8 @@
+import { Logging } from './utils/logging';
+
+/*Logger testing*/
+test('logger prints: [INFO 1] | Logging initialised.', () => {
+    const consoleSpy = jest.spyOn(console, 'log');
+    let logger: Logging = new Logging();
+    expect(consoleSpy).toHaveBeenCalledWith("[INFO 1] | Logging initialised.");
+})
